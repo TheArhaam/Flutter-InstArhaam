f775c7c358e685603c4c75a086cf13a28dd74980
Arhaam Patvi <thearhaam@gmail.com>
2019-12-31 13:29:56 +0530

    Bug Fix

--- lib/userinformation.dart
+++ lib/userinformation.dart
@@ -28,13 +28,9 @@ class UserDisplayDetails {
   String userEmail;
   String userName;
   UserDisplayDetails(this.dpUrl, this.userEmail,this.userName);
-<<<<<<< HEAD:lib/UserInformation.dart
   UserDisplayDetails.fromJSON(Map udd) {
     this.dpUrl = udd['UserDetails']['photoUrl'];
     this.userEmail = udd['UserDetails']['userEmail'];
     this.userName = udd['UserDetails']['userName'];
   }
 }
-=======
-}
->>>>>>> ac1ee8c280a3f6a2b1cc956b6812e87cc1ee7d63:lib/userinformation.dart
